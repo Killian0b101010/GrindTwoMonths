83e9df57d0222a4e85fb9175fe2962ea2ba4d613
Killian0b101010 <teixeira.killian44600@gmail.com>
2024-10-30 16:39:48 +0100

    my_putstr

--- my_putstr.c
+++ my_putstr.c
@@ -6,7 +6,7 @@
 /*   By: killian <killian@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:00:39 by killian           #+#    #+#             */
-/*   Updated: 2024/10/30 11:19:50 by killian          ###   ########.fr       */
+/*   Updated: 2024/10/30 16:37:06 by killian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -26,11 +26,36 @@ void	my_putstr(char *str)
 		my_putchar(str[i]);
 		i++;
 	}
+	my_putchar('\n');
 }
+
+int	my_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+void	my_putnbr(int count)
+{
+	if (count >= 10)
+	{
+		my_putnbr(count / 10);
+	}
+	my_putchar((count % 10) + '0');
+}
+
 int	main(void)
 
 {
 	char *str = "Bon courage";
 	my_putstr(str);
+	int count = my_strlen(str);
+	my_putnbr(count);
 	return (0);
 }
